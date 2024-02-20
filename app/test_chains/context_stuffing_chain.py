1862687d68ce58ac01ad83851adc5459ea0ae9f1
Lance Martin <lance@langchain.dev>
2024-02-20 15:12:01 -0800

    Add LangGraph, cleanup old / redundant chains

--- app/test_chains/context_stuffing_chain.py
+++ app/test_chains/context_stuffing_chain.py
@@ -1,10 +1,11 @@
 from bs4 import BeautifulSoup as Soup
 from langchain_community.document_loaders.recursive_url_loader import RecursiveUrlLoader
 from langchain_openai import ChatOpenAI
-from langchain.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
+from langchain.prompts import PromptTemplate
 from langchain_core.runnables import RunnablePassthrough
-from langchain_core.pydantic_v1 import BaseModel
+from langchain_core.pydantic_v1 import BaseModel, Field
+from langchain.output_parsers.openai_tools import PydanticToolsParser
+from langchain_core.utils.function_calling import convert_to_openai_tool
 
 # Load LCEL docs
 url = "https://python.langchain.com/docs/expression_language/"
@@ -39,15 +40,41 @@ concatenated_content = "\n\n\n --- \n\n\n".join(
     [doc.page_content for doc in d_reversed]
 )
 
-# Prompt template
-template = """You are a coding assistant with expertise in LCEL, LangChain expression language. Here is a full set of documentation:
-{context}
+## Data model
+class code(BaseModel):
+    """Code output"""
+    prefix: str = Field(description="Description of the problem and approach")
+    imports: str = Field(description="Code block import statements")
+    code: str = Field(description="Code block not including import statements")
 
-Now, answer the user question based on the above provided documentation: {question}
-"""
-prompt = ChatPromptTemplate.from_template(template)
+## LLM
+model = ChatOpenAI(temperature=0, model="gpt-4-0125-preview", streaming=True)
 
-model = ChatOpenAI(temperature=0, model="gpt-4-1106-preview") 
+# Tool
+code_tool_oai = convert_to_openai_tool(code)
+
+# LLM with tool and enforce invocation
+llm_with_tool = model.bind(
+    tools=[convert_to_openai_tool(code_tool_oai)],
+    tool_choice={"type": "function", "function": {"name": "code"}},
+)
+
+# Parser
+parser_tool = PydanticToolsParser(tools=[code])
+
+# Create a prompt template with format instructions and the query
+prompt = PromptTemplate(
+    template = """You are a coding assistant with expertise in LCEL, LangChain expression language. \n 
+        Here is a full set of LCEL documentation: 
+        \n ------- \n
+        {context} 
+        \n ------- \n
+        Answer the user question based on the above provided documentation. \n
+        Ensure any code you provide can be executed with all required imports and variables defined. \n
+        Structure your answer with a description of the code solution. \n
+        Then list the imports. And finally list the functioning code block. \n
+        Here is the user question: \n --- --- --- \n {question}""",
+    input_variables=["question","context"])
 
 chain = (
     {
@@ -55,8 +82,8 @@ chain = (
         "question": RunnablePassthrough(),
     }
     | prompt
-    | model
-    | StrOutputParser()
+    | llm_with_tool
+    | parser_tool
 )
 
 # Add typing for input
